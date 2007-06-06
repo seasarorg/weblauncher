23705af99bd14c48b5237be763a71e1afd6ab2da
taichi <ryushi@gmail.com>
2007-06-06 09:25:31 +0000

    謎のぬるぽ対応。
    git-svn-id: https://www.seasar.org/svn/sandbox/weblauncher/trunk@47 8523766c-5c40-478b-b397-97bd31507e9a

--- org.seasar.weblauncher/src/main/java/org/seasar/weblauncher/Activator.java
+++ org.seasar.weblauncher/src/main/java/org/seasar/weblauncher/Activator.java
@@ -11,6 +11,7 @@ import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.osgi.framework.BundleContext;
 import org.seasar.eclipse.common.util.LogUtil;
+import org.seasar.framework.util.StringUtil;
 import org.seasar.weblauncher.preferences.WebPreferences;
 import org.seasar.weblauncher.preferences.impl.WebPreferencesImpl;
 
@@ -19,92 +20,97 @@ import org.seasar.weblauncher.preferences.impl.WebPreferencesImpl;
  */
 public class Activator extends Plugin {
 
-    // The shared instance
-    private static Activator plugin;
+	// The shared instance
+	private static Activator plugin;
 
-    private static Map urlToProjectCache = new Hashtable();
+	private static Map urlToProjectCache = new Hashtable();
 
-    /**
-     * The constructor
-     */
-    public Activator() {
-        plugin = this;
-    }
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+		plugin = this;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
-     */
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
-        DebugPlugin debug = DebugPlugin.getDefault();
-        debug.addDebugEventListener(new TerminateListener());
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		DebugPlugin debug = DebugPlugin.getDefault();
+		debug.addDebugEventListener(new TerminateListener());
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
-     */
-    public void stop(BundleContext context) throws Exception {
-        plugin = null;
-        super.stop(context);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
 
-    /**
-     * Returns the shared instance
-     * 
-     * @return the shared instance
-     */
-    public static Activator getDefault() {
-        return plugin;
-    }
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
 
-    public static WebPreferences getPreferences(IProject project) {
-        return new WebPreferencesImpl(project);
-    }
+	public static WebPreferences getPreferences(IProject project) {
+		return new WebPreferencesImpl(project);
+	}
 
-    public static void log(String msg) {
-        LogUtil.log(getDefault(), msg);
-    }
+	public static void log(String msg) {
+		LogUtil.log(getDefault(), msg);
+	}
 
-    public static void log(Throwable throwable) {
-        LogUtil.log(getDefault(), throwable);
-    }
+	public static void log(Throwable throwable) {
+		LogUtil.log(getDefault(), throwable);
+	}
 
-    public static void setLaunch(IProject project, ILaunch launch) {
-        try {
-            if (project != null) {
-                project.setSessionProperty(Constants.KEY_SERVER_STATE, launch);
-            }
-        } catch (CoreException e) {
-            log(e);
-        }
-    }
+	public static void setLaunch(IProject project, ILaunch launch) {
+		try {
+			if (project != null) {
+				project.setSessionProperty(Constants.KEY_SERVER_STATE, launch);
+			}
+		} catch (CoreException e) {
+			log(e);
+		}
+	}
 
-    public static ILaunch getLaunch(IProject project) {
-        ILaunch result = null;
-        try {
-            if (project != null) {
-                result = (ILaunch) project
-                        .getSessionProperty(Constants.KEY_SERVER_STATE);
-            }
-        } catch (CoreException e) {
-            log(e);
-        }
-        return result;
-    }
+	public static ILaunch getLaunch(IProject project) {
+		ILaunch result = null;
+		try {
+			if (project != null) {
+				result = (ILaunch) project
+						.getSessionProperty(Constants.KEY_SERVER_STATE);
+			}
+		} catch (CoreException e) {
+			log(e);
+		}
+		return result;
+	}
 
-    public static void entry(IProject project, URL url) {
-        urlToProjectCache.put(url.toExternalForm(), project);
-    }
+	public static void entry(IProject project, URL url) {
+		urlToProjectCache.put(url.toExternalForm(), project);
+	}
 
-    public static IProject findProject(String url) {
-        return (IProject) urlToProjectCache.get(url);
-    }
+	public static IProject findProject(String url) {
+		if (StringUtil.isEmpty(url) == false) {
+			return (IProject) urlToProjectCache.get(url);
+		}
+		return null;
+	}
 
-    public static void exit(String url) {
-        urlToProjectCache.remove(url);
-    }
+	public static void exit(String url) {
+		if (StringUtil.isEmpty(url) == false) {
+			urlToProjectCache.remove(url);
+		}
+	}
 }
